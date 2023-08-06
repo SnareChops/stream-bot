361899396677b66bb7d40f3c31823364cb7b4bc4
SnareChops <snare_chops@yahoo.com>
2023-08-06 04:07:34 -0400

    Removed sudo from docker calls in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-sudo docker build --target release -t ghcr.io/snarechops/stream-bot:$GITHUB_RUN_ID .
-echo $GITHUB_TOKEN | sudo docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-sudo docker push ghcr.io/snarechops/stream-bot:$GITHUB_RUN_ID
+docker build --target release -t ghcr.io/snarechops/stream-bot:$GITHUB_RUN_ID .
+echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
+docker push ghcr.io/snarechops/stream-bot:$GITHUB_RUN_ID
