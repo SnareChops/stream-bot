259d8aa2596a91fa405cb8521ccd1f3bfd703a1f
SnareChops <snare_chops@yahoo.com>
2023-08-22 01:31:58 -0400

    Consolidated audio redeems Added new audio redeems

--- redeems/main.go
+++ redeems/main.go
@@ -32,12 +32,30 @@ func ProcessRedeem(signal RedeemSignal) []byte {
 	switch strings.ToLower(signal.Reward.Title) {
 	case "hi":
 		return Hi(signal)
-	case "hooray":
-		return Hooray(signal)
 	case "bass solo":
 		return BassSolo(signal)
+	case "hooray":
+		return Audio("audio/hooray.mp3")
 	case "six hours later":
-		return SixHoursLater(signal)
+		return Audio("audio/six-hours-later.mp3")
+	case "amazing":
+		return Audio("audio/amazing-how-wonderful.mp3")
+	case "disgusting pervert":
+		return Audio("audio/disgusting-pervert-absolutely-unacceptable.mp3")
+	case "i'm bored":
+		return Audio("audio/im-bored.mp3")
+	case "shut up":
+		return Audio("audio/omg-shut-up.mp3")
+	case "meanwhile":
+		return Audio("audio/spy-x-flute.mp3")
+	case "stop it":
+		return Audio("audio/stop-it.mp3")
+	case "that won't work":
+		return Audio("audio/that-wont-work-for-you.mp3")
+	case "very impressive":
+		return Audio("audio/very-impressive-indeed.mp3")
+	case "dummy":
+		return Audio("audio/what-a-complete-moron.mp3")
 	default:
 		return nil
 	}
